d026d4d52c1c82aab9ccdc09e06dab55900b2002
ThomasCarayol <carayol@etud.insa-toulouse.fr>
2013-11-27 15:41:58 +0100

    test

---
+++ src/databasetable/PKOccurrence.java
@@ -0,0 +1,34 @@
+package databasetable;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+
+@Embeddable
+public class PKOccurrence implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Column(name = "idTerme")
+    private int idTerme;
+
+    @Column(name = "idConteneur")
+    private int idConteneur;
+
+	public int getIdTerme() {
+		return idTerme;
+	}
+
+	public void setIdTerme(int idTerme) {
+		this.idTerme = idTerme;
+	}
+
+	public int getIdConteneur() {
+		return idConteneur;
+	}
+
+	public void setIdConteneur(int idConteneur) {
+		this.idConteneur = idConteneur;
+	}
+}
